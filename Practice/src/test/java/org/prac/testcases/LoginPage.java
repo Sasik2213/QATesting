c60b5d806743840bb21bda66d5ec455c4b950c05
SasikiranKakara <55825519+sasikirankakara001@users.noreply.github.com>
2023-11-23 01:09:43 +0530

    commit

--- Practice/src/test/java/org/prac/testcases/LoginPage.java
+++ Practice/src/test/java/org/prac/testcases/LoginPage.java
@@ -1,15 +1,20 @@
 package org.prac.testcases;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.prac.pages.Login;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.github.dockerjava.api.model.Driver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginPage {
@@ -19,10 +24,12 @@ public class LoginPage {
 	@BeforeTest
 	public void startingPoint() {
 		login.launchBrowser();
+		
 	}
-	@AfterTest
+//	@AfterTest
 	public void endingPoint() {
-		login.driver.close();
+//		login.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+		login.driver.quit();
 	}
 
 	@Test
@@ -36,6 +43,7 @@ public class LoginPage {
 		Assert.assertTrue(login.driver.findElement(By.xpath("//h1[normalize-space()='Login']")).isDisplayed());
 		Assert.assertTrue(login.driver.findElement(By.xpath("//label[normalize-space()='USERNAME']")).isDisplayed());
 		Assert.assertTrue(login.driver.findElement(By.xpath("//label[normalize-space()='PASSWORD']")).isDisplayed());
+		login.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
 		login.driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 
 
@@ -58,4 +66,17 @@ public class LoginPage {
 		login.driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 		
 	}
+	@Test
+	public void HomePageTest() throws Exception {
+		/*For Finding the contains H1 tags all over the page
+		 * List<WebElement> h1Tag = login.driver.findElements(By.tagName("h1"));
+		 * for(WebElement x:h1Tag) { System.out.println(x.getText()); }
+		 */
+		LoginCredntialsInvalidAndValidCases();
+		Assert.assertTrue(login.driver.findElement(By.tagName("h1")).isDisplayed());
+		Assert.assertTrue(login.driver.findElement(By.xpath("//button[normalize-space()='Play']")).isDisplayed());
+		login.driver.findElement(By.xpath("//img[@alt='No Time to Die']")).click();
+		
+	}
+	
 }
